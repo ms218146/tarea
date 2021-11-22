d680dec32c8b9fb84860265d58841dc8effa7700
ms218146 <92342308+ms218146@users.noreply.github.com>
2021-11-22 16:57:22 -0300

    Update variables.tf

--- dogs/variables.tf
+++ dogs/variables.tf
@@ -1,23 +1,7 @@
 
-variable "aws_access_key" {
-  type        = string
-  description = "AWS Access Key"
-}
-
-variable "aws_secret_key" {
-  type        = string
-  description = "AWS Secret Key"
-}
-
 variable "aws_region" {
   type        = string
   description = "AWS Region"
-}
-
-variable "aws_cloudwatch_retention_in_days" {
-  type        = number
-  description = "AWS CloudWatch Logs Retention in Days"
-  default     = 1
 }
 
 variable "app_name" {
@@ -30,14 +14,14 @@ variable "ip_red" {
   default     = "10.0.0.0/16"
 }
 
-variable "public_subnets" {
-  description = "List of public subnets"
+variable "subnet_publica" {
+  description = "Lista de subnets publicas"
 }
 
-variable "private_subnets" {
-  description = "List of private subnets"
+variable "subnet_privada" {
+  description = "Lista de subnets privadas"
 }
 
 variable "availability_zones" {
-  description = "List of availability zones"
+  description = "Lista de Zonzas de Disponibilidad"
 }
