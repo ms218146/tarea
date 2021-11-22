c4c4a0cdf39755fd963a3d8d5c0bf6ce761ed50f
ms218146 <92342308+ms218146@users.noreply.github.com>
2021-11-22 16:06:26 -0300

    Update variables.tf

--- cats/variables.tf
+++ cats/variables.tf
@@ -1,28 +1,10 @@
-
-variable "aws_access_key" {
-  type        = string
-  description = "AWS Access Key"
-}
-
-variable "aws_secret_key" {
-  type        = string
-  description = "AWS Secret Key"
-}
-
 variable "aws_region" {
   type        = string
   description = "AWS Region"
 }
-
-variable "aws_cloudwatch_retention_in_days" {
-  type        = number
-  description = "AWS CloudWatch Logs Retention in Days"
-  default     = 1
-}
-
 variable "app_name" {
   type        = string
-  description = "Application Name"
+  description = "Nombre de Aplicacion"
 }
 
 variable "ip_red" {
@@ -30,14 +12,14 @@ variable "ip_red" {
   default     = "10.0.0.0/16"
 }
 
-variable "public_subnets" {
-  description = "List of public subnets"
+variable "subnet_publica" {
+  description = "Lista de subnets publica"
 }
 
-variable "private_subnets" {
-  description = "List of private subnets"
+variable "subnet_privada" {
+  description = "Lista de subnets privada"
 }
 
 variable "availability_zones" {
-  description = "List of availability zones"
+  description = "List de zonas"
 }
